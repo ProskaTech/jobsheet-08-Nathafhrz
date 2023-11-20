5feee18570dfcf180565e226a85dc00c76143f00
Nathafhrz <118335268+Nathafhrz@users.noreply.github.com>
2023-11-20 17:21:09 +0700

    Add files via upload
    created by NathaFahrezi-22346017

---
+++ modul8/src/modul8/tugas1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package modul8;
+
+/**
+ *
+ * @author natha
+ */
+public class tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        String[] days = {"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"};
+
+            for(int i = 0; i < days.length; i++) {
+            System.out.println(days[i]);
+        }
+
+    }
+}
